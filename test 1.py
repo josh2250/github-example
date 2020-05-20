3ce9ac52942d82675992a2a8dc1f9dd218ef24ab
josh2250 <josh2250@hotmail.com>
2020-05-20 14:07:50 -0500

    Project
    Week 1

---
+++ test 1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Project Josue Hernandez 
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy as pd
+
+
+# In[5]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
